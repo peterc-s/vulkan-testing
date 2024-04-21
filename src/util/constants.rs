1ddd68ed8cd269f8ffdcadfe8fe98ce48ebfd202
peterc-s <pjcsmail@gmail.com>
2024-04-21 21:09:48 +0100

    comitting this just in case but it isn't working, going to rewrite

--- src/util/constants.rs
+++ src/util/constants.rs
@@ -1,7 +1,11 @@
 use ash::vk;
+use std::ffi::CStr;
 
 pub const API_VERSION: u32 = vk::make_api_version(0,1,0,0);
 
 pub const WINDOW_TITLE: &'static str = "Vulkan Testing";
 pub const WINDOW_HEIGHT: u32 = 600;
 pub const WINDOW_WIDTH: u32 = 800;
+
+pub const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+pub const VALIDATION_LAYER: &CStr = unsafe { CStr::from_bytes_with_nul_unchecked(b"VK_LAYER_KHRONOS_validation\0") };
