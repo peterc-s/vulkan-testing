f01d03474935bd469f3258d5365b874c218c01fa
peterc-s <pjcsmail@gmail.com>
2024-04-22 21:12:58 +0100

    11. Render passes

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -58,6 +58,7 @@ pub struct AppData {
     pub swapchain_images: Vec<vk::Image>,
     pub swapchain_image_views: Vec<vk::ImageView>,
     pub pipeline_layout: vk::PipelineLayout,
+    pub render_pass: vk::RenderPass,
 }
 
 impl App {
@@ -98,6 +99,9 @@ impl App {
 
         /* swapchain image views */
         create_swapchain_image_views(&device, &mut data)?;
+
+        /* render pass*/
+        create_render_pass(&instance, &device, &mut data)?;
 
         /* pipeline */
         create_pipeline(&device, &mut data)?;
@@ -457,6 +461,41 @@ fn create_swapchain_image_views(
     Ok(())
 }
 
+fn create_render_pass(
+    //instance: &Instance,
+    device: &Device,
+    data: &mut AppData,
+) -> Result<()> {
+    let color_attachment = vk::AttachmentDescription::default()
+        .format(data.swapchain_format)
+        .samples(vk::SampleCountFlags::TYPE_1)
+        .load_op(vk::AttachmentLoadOp::CLEAR)
+        .store_op(vk::AttachmentStoreOp::STORE)
+        .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
+        .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
+        .initial_layout(vk::ImageLayout::UNDEFINED)
+        .final_layout(vk::ImageLayout::PRESENT_SRC_KHR);
+
+    let color_attachment_ref = vk::AttachmentReference::default()
+        .attachment(0)
+        .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
+
+    let color_attachments = &[color_attachment_ref];
+    let subpass = vk::SubpassDescription::default()
+        .pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS)
+        .color_attachments(color_attachments);
+
+    let attachments = &[color_attachment];
+    let subpasses = &[subpass];
+    let info = vk::RenderPassCreateInfo::default()
+        .attachments(attachments)
+        .subpasses(subpasses);
+
+    data.render_pass = unsafe { device.create_render_pass(&info, None)? };
+
+    Ok(())
+}
+
 fn create_pipeline(
     device: &Device,
     data: &mut AppData,
@@ -568,6 +607,7 @@ impl Drop for App {
     fn drop(&mut self) {
         unsafe {
             self.device.destroy_pipeline_layout(self.data.pipeline_layout, None);
+            self.device.destroy_render_pass(self.data.render_pass, None);
 
             self.device.device_wait_idle().unwrap();
 
