21f253da06709b54030ceea720c063ccd69f151f
peterc-s <pjcsmail@gmail.com>
2024-04-23 00:34:38 +0100

    include license and credit to vulkanalia

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -12,6 +12,24 @@ pub unsafe fn string_from_utf8(string: &[i8; 256]) -> String {
                                   .collect::<Vec<_>>()).to_string()
 }
 
+
+
+// Copyright 2024 Kyle Mayes
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+// the below Bytecode struct is modified by me, sourced from https://github.com/KyleMayes/vulkanalia/blob/master/vulkanalia/src/bytecode.rs.
+
 #[derive(Debug)]
 pub struct Bytecode(*mut u8, usize);
 
